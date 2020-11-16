f89c9e18b448899d97578a085b9f1e18dd654645
Nickolay Ermakov <kolian-x@mail.ru>
2020-11-16 16:04:55 +0300

    Basic rain effect

--- WeatherTimeTravel/TimeTravelViewController.swift
+++ WeatherTimeTravel/TimeTravelViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import SpriteKit
 
 class TimeTravelViewController: UIViewController {
 
+    let scene = SKScene()
+    let effectsView = SKView()
+    let emitterNode = SKEmitterNode(fileNamed: "Rain.sks")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let picker = ImmediatePickView()
@@ -32,6 +37,28 @@ class TimeTravelViewController: UIViewController {
                 self?.view.backgroundColor = UIColor(white: CGFloat(index) / CGFloat(elementsCount), alpha: 1.0)
             }
         }, onError: nil, onCompleted: nil, onDisposed: nil)
+        
+        effectsView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(effectsView)
+        NSLayoutConstraint.activate([
+            effectsView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            effectsView.bottomAnchor.constraint(equalTo: picker.topAnchor),
+            effectsView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            effectsView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+        ])
+        effectsView.backgroundColor = .clear
+        scene.scaleMode = .fill
+        scene.backgroundColor = .clear
+        effectsView.presentScene(scene)
+        effectsView.isUserInteractionEnabled = false
+        scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        scene.addChild(emitterNode)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        scene.size = effectsView.frame.size
+        emitterNode.position.y = scene.frame.maxY
+        emitterNode.particlePositionRange.dx = scene.frame.width
     }
 }
 
